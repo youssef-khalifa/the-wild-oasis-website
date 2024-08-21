de81d17e124ce5b970fcac45a292b3c499e2b828
youssef-khalifa <ykhalifa19921@gmail.com>
2024-08-22 02:52:09 +0300

    s

---
+++ app/account/reservations/page.js
@@ -0,0 +1,34 @@
+import ReservationCard from "@/app/_components/ReservationCard";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Reservations",
+};
+
+export default function Page() {
+  // CHANGE
+  const bookings = [];
+
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Your reservations
+      </h2>
+
+      {bookings.length === 0 ? (
+        <p className="text-lg">
+          You have no reservations yet. Check out our{" "}
+          <Link className="underline text-accent-500" href="/cabins">
+            luxury cabins &rarr;
+          </Link>
+        </p>
+      ) : (
+        <ul className="space-y-6">
+          {bookings.map((booking) => (
+            <ReservationCard booking={booking} key={booking.id} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,35 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
