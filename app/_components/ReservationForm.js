b37120cd8683a3f99f34520e47076a5b7aed4bfc
youssef-khalifa <ykhalifa19921@gmail.com>
2024-08-29 00:29:46 +0300

    we

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -2,7 +2,7 @@
 
 import { useReservation } from "./ReservationContext";
 
-function ReservationForm({ cabin }) {
+function ReservationForm({ cabin, user }) {
   const { range } = useReservation();
   const { maxCapacity } = cabin;
 
@@ -11,28 +11,24 @@ function ReservationForm({ cabin }) {
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
 
-        {/* <div className='flex gap-4 items-center'>
+        <div className="flex gap-4 items-center">
           <img
             // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
+            referrerPolicy="no-referrer"
+            className="h-8 rounded-full"
             src={user.image}
             alt={user.name}
           />
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
-
-      <p>
-        {String(range.from)} to {String(range.to)}
-      </p>
 
       <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
         <div className="space-y-2">
-          <label htmlFor="numberGuests">How many guests?</label>
+          <label htmlFor="numGuests">How many guests?</label>
           <select
-            name="numberGuests"
-            id="numberGuests"
+            name="numGuests"
+            id="numGuests"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             required
           >
--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,17 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Jonas
+      Welcome, {firstName}
     </h2>
   );
 }
